5635a87189a3c68c29dae9e36f25f9b55e85ec15
Eudo08 <e2khovrine92@gmail.com>
2024-12-02 11:23:14 +0100

    Update data.py

--- data.py
+++ data.py
@@ -1,1 +1,45 @@
+# exemple dico rempli
+eleves = {
+    "élève 1" : {
+        "info perso" : {
+            "nom" : "abc",
+            "age" : "16",
+            "genre" : "femme"
+        },
+        "préférences" : {
+            "couleur" : "violet",
+            "plat" : "tacos",
+            "matière préférée" : "NSI"
 
+        },
+        "loisirs" : {
+            "sport" : "hockey",
+            "musique " : "pop",
+            "jeux vidéo" : "polytopia",
+            "lecture" : "fantastique"
+        }
+    }   
+}
+
+# exemple dico vide
+eleves = {
+    "élève 1" : {
+        "info perso" : {
+            "nom" : "",
+            "age" : "",
+            "genre" : ""
+        },
+        "préférences" : {
+            "couleur" : "",
+            "plat" : "",
+            "matière préférée" : ""
+
+        },
+        "loisirs" : {
+            "sport" : "",
+            "musique " : "",
+            "jeux vidéo" : "",
+            "lecture" : ""
+        }
+    }   
+}
