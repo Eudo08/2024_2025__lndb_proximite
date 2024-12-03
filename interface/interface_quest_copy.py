d3833ac260625b81b7938107dfc16378d9d10caf
Eudo08 <e2khovrine92@gmail.com>
2024-12-03 09:55:02 +0100

    flask

--- interface/interface_quest_copy.py
+++ interface/interface_quest_copy.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 site = Flask(__name__)
 
 @site.route("/")
 def bonjour() : 
-    return "<p>Bienvenue </p>"
+    return render_template("index.html")
 
 if __name__ == '__main__':
     site.run(debug=True)
