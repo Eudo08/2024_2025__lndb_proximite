25ce434407de3c02a62819a400f40d066f5f9b08
louisechristophe <louisechristophe08@gmail.com>
2024-12-03 09:09:13 +0100

    1st update

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+dico = {
+    "person_1": {
+        "prenom" : "Eudocie",
+        "nom" : "De Khovrine",
+        "interet" : "manger"
+        },
+    "person_2":{
+        "prenom" : "Marine",
+        "nom" : "Fraboulet",
+        "interet" : "dormir"
+        },
+    "person_3" : {
+        "prenom" : "Louise",
+        "nom" : "Christophe",
+        "interet" : "dormir"
+    }
+}
 
+# def compare_with_different_person ():
+#     liste = list(dico.keys)
+#     for person in dico.keys:
+#         if dico [person]["interet"] 
+
+liste = list(dico.keys)
+print (liste)
