ea2b0661694fad6ae00f6183f387e031bd1b4ba5
louisechristophe <louisechristophe08@gmail.com>
2024-12-09 11:46:29 +0100

    data insertion dico

--- main.py
+++ main.py
@@ -1,39 +1,4 @@
-dico = {
-    "person_1": {
-        "presentation" : {
-        "prenom" : "Eudocie",
-        "nom" : "De Khovrine"
-        },
-        "info_perso" : {
-        "interet" : "manger",
-        "age" : "16 ans",
-        "taille" : "110/130 cm"
-        }
-        },
-    "person_2":{
-        "presentation" : {
-        "prenom" : "Marine",
-        "nom" : "Fraboulet"
-        },
-        "info_perso" : {
-        "interet" : "dormir",
-        "age" : "16 ans",
-        "taille" : "110/130 cm"
-        }
-        },
-    "person_3" : {
-        "presentation" : {
-        "prenom" : "Louise",
-        "nom" : "Christophe"
-        },
-        "info_perso" : {
-        "interet" : "dormir",
-        "age" : "16 ans",
-        "taille" : "160/180 cm"
-        }
-    }
-}
-
+from data import *
 dico_compatibilite = {}
 
 def calcul_compatibilite (cptr,person):
